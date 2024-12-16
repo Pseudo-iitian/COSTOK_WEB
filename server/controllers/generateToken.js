07f50d5a5c9c0b6e7965da0230734d5035a25e27
Abhishek Verma <vermaabhimanyu593@gmail.com>
2024-12-16 14:15:28 +0530

    set token expiry to 1 day as of now

--- server/controllers/generateToken.js
+++ server/controllers/generateToken.js
@@ -6,9 +6,9 @@ function generateToken(user) {
   if (!secret) {
     throw new Error("JWT_SECRET is not defined in environment variables");
   }
-  return jwt.sign(payload, secret, { expiresIn: "1h" });
+  return jwt.sign(payload, secret, { expiresIn: "1d" });
 }
 
 module.exports = {
   generateToken,
-}
+};
