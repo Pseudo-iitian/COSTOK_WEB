540708809dd13ed99960e40f3041b9fadd14c166
Abhishek Verma <109858266+Pseudo-iitian@users.noreply.github.com>
2024-12-30 17:59:01 +0000

    done

--- server3/models/db.js
+++ server3/models/db.js
@@ -2,15 +2,13 @@ const mongoose = require("mongoose");
 
 const mongoURI = "mongodb+srv://vermaabhimanyu593:MniLtQC4XZ84rIDE@costok.mcz6c.mongodb.net/BMS";
 
-// Define connection options
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  connectTimeoutMS: 60000,  // 60 seconds timeout for initial connection
-  socketTimeoutMS: 60000,   // 60 seconds timeout for socket inactivity
+  connectTimeoutMS: 60000,  // 60 seconds timeout
+  socketTimeoutMS: 60000,   // 60 seconds socket timeout
 };
 
-// Establish MongoDB connection with options
 mongoose.connect(mongoURI, options)
   .then(() => {
     console.log("MongoDB connected successfully");
