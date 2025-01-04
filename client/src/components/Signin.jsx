ef0108b3a44d46f9f6fd0ff58df23d5a67d430ce
Abhishek Verma <109858266+Pseudo-iitian@users.noreply.github.com>
2025-01-04 06:42:57 +0000

    changed placeholder for username

--- client/src/components/Signin.jsx
+++ client/src/components/Signin.jsx
@@ -63,7 +63,7 @@ export function Signin() {
                 type="text"
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-krishna-blue-500 focus:border-krishna-blue-500 focus:z-10 sm:text-sm"
-                placeholder="Username"
+                placeholder="Phone Number"
                 onChange={(e) => setUsername(e.target.value)}
               />
             </div>
--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -83,7 +83,7 @@ export function Signup() {
                 type="text"
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-krishna-blue-500 focus:border-krishna-blue-500 focus:z-10 sm:text-sm"
-                placeholder="Username"
+                placeholder="Phone Number"
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
               />
